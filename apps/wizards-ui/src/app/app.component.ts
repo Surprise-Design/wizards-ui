b260b2e7165411e4d6dcc15e42ab95303194068f
saruman <saruman@saruman.com>
2024-03-06 14:01:29 +0100

    hotfix

--- apps/wizards-ui/src/app/app.component.ts
+++ apps/wizards-ui/src/app/app.component.ts
@@ -11,8 +11,21 @@ import { InputTextComponent } from '@wizards-ui/ui'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  options = testOptions;
   title = 'wizards-ui';
   form = new FormGroup({
-    test: new FormControl<string>('1')
+    test: new FormControl<string>('1'),
+    testSelect: new FormControl<string | null>(null)
   })
 }
+
+export const testOptions: any[] = [
+  {
+    label: 'Tak',
+    value: 'yes',
+  },
+  {
+    label: 'Nie',
+    value: 'no',
+  },
+];
