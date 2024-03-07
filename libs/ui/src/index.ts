f20ed1bc187024df4cbae9be285f30722419bebe
saruman <saruman@saruman.com>
2024-03-07 23:25:19 +0100

    updated index.ts

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,2 +1,4 @@
 export * from './input-text/input-text.component'
 export * from './select-input/select-input.component'
+export * from './checkbox/checkbox.component'
+export * from './datepicker/datepicker.component'
